9d530fe5746cc3643acc5bf06303b2600f577901
Chhaya <chhayasingh3008@gmail.com>
2026-07-11 23:49:44 +0530

    Week4 - Module9 Microservices - added eureka-server, api-gateway, screenshots and README

--- Week4/Module9-Microservices/account-service/src/main/java/com/cognizant/account/AccountServiceApplication.java
+++ Week4/Module9-Microservices/account-service/src/main/java/com/cognizant/account/AccountServiceApplication.java
@@ -4,14 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountServiceApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AccountServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
-        LOGGER.info("Account Service Started");
+        LOGGER.info("Account Service Started and Registered with Eureka");
     }
 }
--- Week4/Module9-Microservices/loan-service/src/main/java/com/cognizant/loan/LoanServiceApplication.java
+++ Week4/Module9-Microservices/loan-service/src/main/java/com/cognizant/loan/LoanServiceApplication.java
@@ -4,14 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LoanServiceApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoanServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(LoanServiceApplication.class, args);
-        LOGGER.info("Loan Service Started");
+        LOGGER.info("Loan Service Started and Registered with Eureka");
     }
 }
